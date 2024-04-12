3ca2104220c8ec842d442efbb683cad3568e04c2
Zero-definition <saathvikchoudhary@gmail.com>
2024-04-12 17:26:19 +0530

    Basic react structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import AssetSummary from './AssetSummary'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div className="App-heading">
+          <p children className="heading">A$$ets</p>
+        </div>
+        
+        <AssetSummary/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AssetSummary.js
@@ -0,0 +1,23 @@
+import './AssetSummary.css'
+
+function AssetSummary(){
+    return(
+        <div className="Summary-Container">
+            <div className="AssetCountSummary">
+                
+            </div>
+
+            <div className="CostSummary">
+
+            </div>
+
+            <div className="CurrentValueSummary">
+
+            </div>
+
+            <p className="content">sdf</p>
+        </div>
+    )
+}
+
+export default AssetSummary;
